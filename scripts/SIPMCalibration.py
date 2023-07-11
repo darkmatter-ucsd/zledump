8c797c6751be677037c8a2afe42dd4b24e3ea1aa
yuema137 <3124558229@qq.com>
2023-07-12 02:22:16 +0800

    add the script for sipm test

---
+++ scripts/SIPMCalibration.py
@@ -0,0 +1,58 @@
+import subprocess as subp
+import sys as sys
+import time as time
+import numpy as np
+import os
+
+"""
+example:
+    python3 SIPMCalibration.py /home/waterbarque/sipm_test/zledump/sipm_test_config.txt 10000 522 -98 52
+"""
+
+try:
+    ConfigFile = sys.argv[1]
+    EventNum = int(sys.argv[2])
+    SipmNum = int(sys.argv[3])
+    Temperature = int(sys.argv[4])
+    Voltage = np.round(float(sys.argv[5]))
+except Exception as e:
+    print("Error:",e)
+    print("""Missing input. Please use the script in this way:
+python3 SIPMCalibration.py [ConfigFile] [EventNum] [SipmNum] [Temperature] [Voltage]""")
+
+store_dir = "/home/waterbarque/sipm_test/sipm_data"
+current_folder = f"sipm_{SipmNum}"
+
+full_output_dir = os.path.join(store_dir,current_folder)
+if not os.path.exists(full_output_dir):
+    os.mkdir(full_output_dir, mode=0o777)
+    print(f"folder {full_output_dir} doesn't exist, just created")
+else:
+    print(f"folder {full_output_dir} already exists")
+
+tag = f"sipm{SipmNum}_{Temperature}C_{Voltage}V_{EventNum}"
+print(f"tag is {tag}")
+command = f"./../build/zle_exe {ConfigFile} {EventNum} {full_output_dir}/ {tag}"
+log_file = open(f"{full_output_dir}/log.txt","a")
+
+subp.call(f"{command}",shell=True)
+
+"""
+process = subp.Popen(command,shell=True,stdout=subp.PIPE,stderr=subp.STDOUT)
+while True:
+    # read the information from the process
+    line = process.stdout.readline()
+    if not line:
+        break
+    # print the information in terminal
+    sys.stdout.write(line.decode())
+    sys.stdout.flush()
+
+    # add the information to log_file
+    log_file.write(line.decode())
+    log_file.flush()
+log_file.close()
+"""
+
+time.sleep(2)
+
